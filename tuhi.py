08ab047d0ae01888303ae4adb1f7d437d913ccaa
Peter Hutterer <peter.hutterer@who-t.net>
2018-01-23 14:12:08 +1000

    tuhi: set pairing to False on known devices too

--- tuhi.py
+++ tuhi.py
@@ -289,6 +289,7 @@ class Tuhi(GObject.Object):
         d = self.devices[bluez_device.address]
 
         if Tuhi._is_pairing_device(bluez_device):
+            d.paired = False
             logger.debug('{}: call Pair() on device'.format(bluez_device.objpath))
         elif d.listening:
             d.connect_device()
