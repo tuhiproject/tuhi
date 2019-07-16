6e21db0c9a46a48cd50f9d244776a02555dc2e01
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-16 13:15:40 +1000

    Revert "Store the SVGs in the runtime directory"
    Actually, not a good idea since we can't rely on Tuhi to store the drawings
    forever.
    
    This reverts commit 5470a7afd23c44d55ee14d32ddf081a8e0eab251.

--- tuhigui/svg.py
+++ tuhigui/svg.py
@@ -19,7 +19,7 @@ import xdg.BaseDirectory
 import svgwrite
 import os
 
-DATA_PATH = os.path.join(xdg.BaseDirectory.get_runtime_dir(), 'tuhigui')
+DATA_PATH = os.path.join(xdg.BaseDirectory.xdg_data_home, 'tuhigui')
 
 
 class JsonSvg(GObject.Object):
