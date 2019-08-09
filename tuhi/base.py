tuhiproject/tuhi

commit e1a3675439116c00a5bd5e14b5b539b0193db59e

base: ignore anything not from the known vendor IDs
We don't want to accidentally start talking to a device and brick it.

tuhi/base.py
@@ -333,9 +333,6 @@ class Tuhi(GObject.Object):
 
     @classmethod
     def _device_in_register_mode(cls, bluez_device):
-        if bluez_device.vendor_id not in WACOM_COMPANY_IDS:
-            return False
-
         # When the device is in register mode (blue light blinking), the
         # manufacturer is merely 4 bytes. This will reset to 7 bytes even
         # when the device simply times out and does not register fully.
@@ -368,16 +365,17 @@ class Tuhi(GObject.Object):
             stage.
         '''
 
-        uuid = None
+        # We have a reverse-engineered protocol. Let's not talk to anyone
+        # who doesn't look like we know them to avoid potentially bricking a
+        # device.
+        if bluez_device.vendor_id not in WACOM_COMPANY_IDS:
+            return
 
         # check if the device is already known to us
         try:
             config = self.config.devices[bluez_device.address]
             uuid = config['uuid']
         except KeyError:
-            pass
-
-        if uuid is None and bluez_device.vendor_id not in WACOM_COMPANY_IDS:
             return
 
         # if we got here from a currently live BlueZ device,

commit 318bc211d5e4c290ac634e176a89d96102e54dd9

base: fix failing registration of new devices
Regression introduced in e1a3675439116c00a5bd5e14b5b539b0193db59e

Where a device has no config entry, the e1a367 change would return early. This
is fine for devices previously seen and still lingering in the config but for
completely new devices, we'll never get past that KeyError.

Return to the old behaviour: where we don't have a stored UUID we continue
with the None UUID.

Fixes #148

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

tuhi/base.py
@@ -370,7 +370,7 @@ class Tuhi(GObject.Object):
             config = self.config.devices[bluez_device.address]
             uuid = config['uuid']
         except KeyError:
-            return
+            uuid = None
 
         # if we got here from a currently live BlueZ device,
         # ManufacturerData is reliable. Else, consider the device not in