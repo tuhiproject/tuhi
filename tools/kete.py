tuhiproject/tuhi

commit cc7ba1b365f55436db5eeb8ffd9f6df50015ee50

kete: move the ini file template out of the code
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

tools/kete.py
@@ -33,6 +33,27 @@ from pathlib import Path
 
 CONFIG_PATH = Path(xdg.BaseDirectory.xdg_data_home, 'tuhi-kete')
 
+INI_TEMPLATE = \ # noqa
+'''
+# configuration file for kete
+
+# the file follows a standard .ini format:
+# each device should have its own section like the following
+# [Bluetooth Address]
+# in each section, possible keys are:
+# - Orientation (possible values: Portrait, Landscape,
+#                                 Reverse-Portrait, Reverse-Landscape
+#               defaults to Landscape)
+# - HandlePressure (possible values: true, false
+#                   defaults to false)
+
+
+# Example:
+[11:22:33:44:55:66]
+Orientation = Reverse-Portrait
+HandlePressure = true
+'''
+
 
 class ColorFormatter(logging.Formatter):
     BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, LIGHT_GRAY = range(30, 38)
@@ -769,25 +790,7 @@ class TuhiKeteShell(cmd.Cmd):
         else:
             # Populate config file with a configuration example
             with open(self._config_file, 'w') as f:
-                f.write('''# configuration file for kete
-
-# the file follows a standard .ini format:
-# each device should have its own section like the following
-# [Bluetooth Address]
-# in each section, possible keys are:
-# - Orientation (possible values: Portrait, Landscape,
-#                                 Reverse-Portrait, Reverse-Landscape
-#               defaults to Landscape)
-# - HandlePressure (possible values: true, false
-#                   defaults to false)
-
-
-# Example:
-[11:22:33:44:55:66]
-Orientation = Reverse-Portrait
-HandlePressure = true
-
-''')
+                f.write(INI_TEMPLATE)
 
         self._history_file = Path(CONFIG_PATH, 'histfile')
 

commit e40bdeeebe43a2cbadb1ffa7989b1ef1ba0080e4

tools: fix syntax error in kete
Introduced in cc7ba1b365f55436db5eeb8ffd9f6df50015ee50 for slightly
overzealous flake8 handling.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

tools/kete.py
@@ -33,8 +33,7 @@ from pathlib import Path
 
 CONFIG_PATH = Path(xdg.BaseDirectory.xdg_data_home, 'tuhi-kete')
 
-INI_TEMPLATE = \ # noqa
-'''
+INI_TEMPLATE = '''
 # configuration file for kete
 
 # the file follows a standard .ini format: